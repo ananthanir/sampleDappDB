02fe070cb5e436aaacd026269cdc1b2bd1ba7896
Ananthan <ananthan.rajasekharan@gmail.com>
2024-12-17 18:38:48 +0530

    fixed the send tx and get events

--- getevent/main.js
+++ getevent/main.js
@@ -1,16 +1,24 @@
 import { id, Interface, JsonRpcProvider } from 'ethers'
 
-const provider = new JsonRpcProvider('http://127.0.0.1:8545')
-const eventTopic = id('Datas(string[])')
+const provider = new JsonRpcProvider('http://192.168.3.104:8545')
+const eventTopic = id('Datas(string,string[])')
+
+const courseTopic = id('A')
 
 const certABI = [
   {
     "anonymous": false,
     "inputs": [
       {
+        "indexed": true,
+        "internalType": "string",
+        "name": "examno",
+        "type": "string"
+      },
+      {
         "indexed": false,
         "internalType": "string[]",
-        "name": "",
+        "name": "data",
         "type": "string[]"
       }
     ],
@@ -20,7 +28,7 @@ const certABI = [
 ]
 const iface = new Interface(certABI)
 
-const certAddress = "0xa50a51c09a5c451C52BB714527E1974b686D8e77"
+const certAddress = "0x42aeb727C3D7E4eF8ccde8039bDF9bE804B3B9FF"
 
 
 let eventlogs = []
@@ -35,10 +43,12 @@ const getLogsPromise = provider.getLogs({
   fromBlock: 'earliest',
   toBlock: 'latest',
   address: certAddress,
-  topics: [eventTopic],
+  topics: [eventTopic, courseTopic],
 });
 
 const [logs1, logs2] = await Promise.all([getLogsPromise, getLogsPromise]);
+
+console.log(logs1.length);
 
 logs1.forEach((log) => {
   eventlogs.push(iface.parseLog(log));
--- getevent/sendData.js
+++ getevent/sendData.js
@@ -3,82 +3,88 @@ import fs from 'fs';
 import csv from 'csv-parser';
 
 const ABI = [
-    {
-      "anonymous": false,
-      "inputs": [
-        {
-          "indexed": false,
-          "internalType": "string[]",
-          "name": "",
-          "type": "string[]"
-        }
-      ],
-      "name": "Datas",
-      "type": "event"
-    },
-    {
-      "inputs": [],
-      "name": "getAllData",
-      "outputs": [
-        {
-          "internalType": "string[][]",
-          "name": "",
-          "type": "string[][]"
-        }
-      ],
-      "stateMutability": "view",
-      "type": "function"
-    },
-    {
-      "inputs": [
-        {
-          "internalType": "uint256",
-          "name": "index",
-          "type": "uint256"
-        }
-      ],
-      "name": "getData",
-      "outputs": [
-        {
-          "internalType": "string[]",
-          "name": "",
-          "type": "string[]"
-        }
-      ],
-      "stateMutability": "view",
-      "type": "function"
-    },
-    {
-      "inputs": [
-        {
-          "internalType": "string[][]",
-          "name": "newStrings",
-          "type": "string[][]"
-        }
-      ],
-      "name": "storeData",
-      "outputs": [],
-      "stateMutability": "nonpayable",
-      "type": "function"
-    },
-    {
-      "inputs": [
-        {
-          "internalType": "string[][]",
-          "name": "newStrings",
-          "type": "string[][]"
-        }
-      ],
-      "name": "storeDatas",
-      "outputs": [],
-      "stateMutability": "nonpayable",
-      "type": "function"
-    }
-  ]
+  {
+    "anonymous": false,
+    "inputs": [
+      {
+        "indexed": true,
+        "internalType": "string",
+        "name": "examno",
+        "type": "string"
+      },
+      {
+        "indexed": false,
+        "internalType": "string[]",
+        "name": "data",
+        "type": "string[]"
+      }
+    ],
+    "name": "Datas",
+    "type": "event"
+  },
+  {
+    "inputs": [],
+    "name": "getAllData",
+    "outputs": [
+      {
+        "internalType": "string[][]",
+        "name": "",
+        "type": "string[][]"
+      }
+    ],
+    "stateMutability": "view",
+    "type": "function"
+  },
+  {
+    "inputs": [
+      {
+        "internalType": "uint256",
+        "name": "index",
+        "type": "uint256"
+      }
+    ],
+    "name": "getData",
+    "outputs": [
+      {
+        "internalType": "string[]",
+        "name": "",
+        "type": "string[]"
+      }
+    ],
+    "stateMutability": "view",
+    "type": "function"
+  },
+  {
+    "inputs": [
+      {
+        "internalType": "string[][]",
+        "name": "newStrings",
+        "type": "string[][]"
+      }
+    ],
+    "name": "storeData",
+    "outputs": [],
+    "stateMutability": "nonpayable",
+    "type": "function"
+  },
+  {
+    "inputs": [
+      {
+        "internalType": "string[][]",
+        "name": "newStrings",
+        "type": "string[][]"
+      }
+    ],
+    "name": "storeDatas",
+    "outputs": [],
+    "stateMutability": "nonpayable",
+    "type": "function"
+  }
+]
 
-const address = "0xa50a51c09a5c451C52BB714527E1974b686D8e77";
+const address = "0x42aeb727C3D7E4eF8ccde8039bDF9bE804B3B9FF";
 
-const provider = new JsonRpcProvider("http://127.0.0.1:8545");
+const provider = new JsonRpcProvider("http://192.168.3.104:8545");
 const wallet = new Wallet('8f2a55949038a9610f50fb23b5883af3b4ecb3c3bb792cbcefbd1542c692be63', provider);
 
 const instance = new Contract(address, ABI, wallet);
@@ -103,8 +109,12 @@ async function readCSV() {
 let datas = await readCSV();
 console.log("Sending data to blockchain");
 
-for (let i = 0; i < datas.length; i += 1000) {
-  const chunk = datas.slice(i, i + 1000);
+// for (let i = 0; i < datas.length; i += 1000) {
+//   const chunk = datas.slice(i, i + 1000);
+//   const trx = await instance.storeDatas(chunk);
+//   console.log(`Inserted chunk ${i / 1000 + 1}`);
+// }
+
+
+  const chunk = datas.slice(0, 1000);
   const trx = await instance.storeDatas(chunk);
-  console.log(`Inserted chunk ${i / 1000 + 1}`);
-}
