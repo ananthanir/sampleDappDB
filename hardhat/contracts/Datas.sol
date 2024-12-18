73624c2f9908de7c91edde7d84b93cedc648dbe2
Ananthan <ananthan.rajasekharan@gmail.com>
2024-12-18 18:37:02 +0530

    trying another fix

--- hardhat/contracts/Datas.sol
+++ hardhat/contracts/Datas.sol
@@ -3,22 +3,54 @@ pragma solidity >=0.8.2 <0.9.0;
 
 contract DataStoreMul {
     string[][] stringArray;
+    mapping(string => string[][]) stringMap;
 
-    event Datas(string index, string[]);
+    mapping(string => uint) datacount; // examno => count
+    mapping(string => uint[2]) public eventcount; // examno => [start, end]
+
+    event Datas(string indexed examno, string[] data);
 
     function storeData(string[][] memory newStrings) public {
         stringArray = newStrings;
     }
 
     function storeDatas(string[][] memory newStrings) public {
+
         for (uint i = 0; i < newStrings.length; i++) {
             stringArray.push(newStrings[i]);
-            emit Datas(newStrings[i][1], newStrings[i]);
+            emit Datas(newStrings[i][3], newStrings[i]);
         }
     }
 
-    function getData(uint index) public view returns (string[] memory) {
+    function storeDataMap(string[][] memory newStrings) public {
+        for (uint i = 0; i < newStrings.length; i++) {
+            stringMap[newStrings[i][3]].push(newStrings[i]);
+            emit Datas(newStrings[i][3], newStrings[i]);
+
+            if (eventcount[newStrings[i][3]][0] == 0 && eventcount[newStrings[i][3]][1] == 0) {
+                eventcount[newStrings[i][3]][0] = block.number;
+                eventcount[newStrings[i][3]][1] = block.number;
+            } else {
+                eventcount[newStrings[i][3]][1] = block.number;
+            }
+        }
+    }
+
+    function getDataArray(uint index) public view returns (string[] memory) {
         return stringArray[index];
+    }
+
+    function getDataMap(
+        string memory examno, uint start, uint number
+    ) public view returns (string[][] memory) {
+        string[][] storage data = stringMap[examno];
+        require(start + number <= data.length, "Out of bounds");
+
+        string[][] memory batch = new string[][](number);
+        for (uint i = 0; i < number; i++) {
+            batch[i] = data[start + i];
+        }
+        return batch;
     }
 
     function getAllData() public view returns (string[][] memory) {
--- hardhat/hardhat.config.js
+++ hardhat/hardhat.config.js
@@ -3,10 +3,14 @@ require("@nomicfoundation/hardhat-toolbox");
 /** @type import('hardhat/config').HardhatUserConfig */
 module.exports = {
   solidity: "0.8.27",
-defaultNetwork: "localhost",
+defaultNetwork: "duk",
   networks: {
     localhost: {
       url: "http://127.0.0.1:8545",
+      accounts: ["8f2a55949038a9610f50fb23b5883af3b4ecb3c3bb792cbcefbd1542c692be63"],
+    },
+    duk: {
+      url: "http://192.168.3.104:8545",
       accounts: ["8f2a55949038a9610f50fb23b5883af3b4ecb3c3bb792cbcefbd1542c692be63"],
     },
   }
