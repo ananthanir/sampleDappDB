5c0f05b6532dc6aece67d5c843292533eec3f0cc
Ananthan <ananthan.rajasekharan@gmail.com>
2024-12-19 16:16:31 +0530

    d2

--- getevent/main.js
+++ getevent/main.js
@@ -1,48 +1,82 @@
-import { id, Interface, JsonRpcProvider } from 'ethers'
+import { id, Interface, JsonRpcProvider, Contract, Wallet } from 'ethers'
 
 const provider = new JsonRpcProvider('http://192.168.3.104:8545')
 const eventTopic = id('Datas(string,string[])')
 
-const courseTopic = id('152')
+const courseTopic = id('A')
 
 const certABI = [
-  {
-    "anonymous": false,
-    "inputs": [
-      {
-        "indexed": true,
-        "internalType": "string",
-        "name": "examno",
-        "type": "string"
-      },
-      {
-        "indexed": false,
-        "internalType": "string[]",
-        "name": "data",
-        "type": "string[]"
-      }
-    ],
-    "name": "Datas",
-    "type": "event"
-  }
+    {
+        "inputs": [
+            {
+                "internalType": "string",
+                "name": "",
+                "type": "string"
+            },
+            {
+                "internalType": "uint256",
+                "name": "",
+                "type": "uint256"
+            }
+        ],
+        "name": "eventcount",
+        "outputs": [
+            {
+                "internalType": "uint256",
+                "name": "",
+                "type": "uint256"
+            }
+        ],
+        "stateMutability": "view",
+        "type": "function"
+    },
+    {
+        "anonymous": false,
+        "inputs": [
+            {
+                "indexed": true,
+                "internalType": "string",
+                "name": "examno",
+                "type": "string"
+            },
+            {
+                "indexed": false,
+                "internalType": "string[]",
+                "name": "data",
+                "type": "string[]"
+            }
+        ],
+        "name": "Datas",
+        "type": "event"
+    }
 ]
 const iface = new Interface(certABI)
 
-const certAddress = "0x42aeb727C3D7E4eF8ccde8039bDF9bE804B3B9FF"
+const certAddress = "0x8caa34dE363af3ecD21677845F4508Ebd5F41ba3"
 
 let eventlogs = []
 
 BigInt.prototype.toJSON = function () {
-  return this.toString()
+    return this.toString()
 }
 
 console.time('getLogsDuration');
 
+const wallet = new Wallet('8f2a55949038a9610f50fb23b5883af3b4ecb3c3bb792cbcefbd1542c692be63', provider);
+
+const instance = new Contract(certAddress, certABI, wallet);
+
+const start = await instance.eventcount('A',0);
+const end = await instance.eventcount('A',1);
+
+console.log("Range: ", start, end);
+console.log("Range: ", `0x${start.toString(16).toUpperCase()}`, `0x${end.toString(16).toUpperCase()}`);
+
 const getLogsPromise = provider.getLogs({
-  fromBlock: '0xFA0',
-  toBlock: '0x13FB',
-  address: certAddress,
-  topics: [eventTopic, courseTopic],
+    fromBlock: `0x${start.toString(16).toUpperCase()}`,
+    toBlock: `0x${end.toString(16).toUpperCase()}`,
+    address: certAddress,
+    topics: [eventTopic, courseTopic],
 });
 
 const [logs1, logs2] = await Promise.all([getLogsPromise, getLogsPromise]);
@@ -50,11 +84,11 @@ const [logs1, logs2] = await Promise.all([getLogsPromise, getLogsPromise]);
 console.log(logs1.length);
 
 logs1.forEach((log) => {
-  eventlogs.push(iface.parseLog(log));
+    eventlogs.push(iface.parseLog(log));
 });
 
 logs2.forEach((log) => {
-  eventlogs.push(iface.parseLog(log));
+    eventlogs.push(iface.parseLog(log));
 });
 
 console.log(eventlogs.length);
--- getevent/sendDataMap.js
+++ getevent/sendDataMap.js
@@ -18,7 +18,7 @@ const ABI = [
   },
 ]
 
-const address = "0x5681495692C07a470B59b7Fb160B7a6d2CDB4A2E";
+const address = "0x8caa34dE363af3ecD21677845F4508Ebd5F41ba3";
 
 const provider = new JsonRpcProvider("http://192.168.3.104:8545");
 const wallet = new Wallet('8f2a55949038a9610f50fb23b5883af3b4ecb3c3bb792cbcefbd1542c692be63', provider);
@@ -45,12 +45,12 @@ async function readCSV() {
 let datas = await readCSV();
 console.log("Sending data to blockchain");
 
-for (let i = 0; i < 3; i += 2000) {
-  const chunk = datas.slice(i, i + 2000);
-  const trx = await instance.storeDataMap(chunk);
-  console.log(`Inserted chunk ${i / 2000 + 1}`);
-}
+// for (let i = 0; i < 3; i += 2000) {
+//   const chunk = datas.slice(i, i + 2000);
+//   const trx = await instance.storeDataMap(chunk);
+//   console.log(`Inserted chunk ${i / 2000 + 1}`);
+// }
 
-// const chunk = datas.slice(0, 2000);
-// const trx = await instance.storeDataMap(chunk);
+const chunk = datas.slice(0, 2000);
+const trx = await instance.storeDataMap(chunk);
 console.log("Done :)");
