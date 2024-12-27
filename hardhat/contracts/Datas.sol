4ecff438c3b3510a374abf7c2940ba351b50707e
Ananthan <ananthan.rajasekharan@gmail.com>
2024-12-27 10:46:55 +0530

    fix

--- hardhat/contracts/Datas.sol
+++ hardhat/contracts/Datas.sol
@@ -3,10 +3,17 @@ pragma solidity >=0.8.2 <0.9.0;
 
 contract DataStoreMul {
     string[][] stringArray;
+
+    struct eventinfo {
+        uint start;
+        uint end;
+        bool firstentry;        
+    }
+
     mapping(string => string[][]) stringMap;
 
     mapping(string => uint) datacount; // examno => count
-    mapping(string => uint[2]) public eventcount; // examno => [start, end]
+    mapping(string => eventinfo) public eventcount; // examno => [start, end]
 
     event Datas(string indexed examno, string[] data);
 
@@ -26,12 +33,11 @@ contract DataStoreMul {
         for (uint i = 0; i < newStrings.length; i++) {
             stringMap[newStrings[i][3]].push(newStrings[i]);
             emit Datas(newStrings[i][3], newStrings[i]);
+            eventcount[newStrings[i][3]].end = block.number;
 
-            if (eventcount[newStrings[i][3]][0] == 0 && eventcount[newStrings[i][3]][1] == 0) {
-                eventcount[newStrings[i][3]][0] = block.number;
-                eventcount[newStrings[i][3]][1] = block.number;
-            } else {
-                eventcount[newStrings[i][3]][1] = block.number;
+            if (!eventcount[newStrings[i][3]].firstentry) {
+                eventcount[newStrings[i][3]].start = block.number;
+                eventcount[newStrings[i][3]].firstentry=true;
             }
         }
     }
